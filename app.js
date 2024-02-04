89a426fc26fb684a7a3f6fda3b0c024da43283c5
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-04 19:23:13 +0530

    make request and response

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const http = require("http")
 
 const server = http.createServer((req,res)=>{
     console.log("Sumit Singh Tomar")
-    res.write("hello sumit singh tomar")
+    // res.write("hello sumit singh tomar")
+    process.exit()
 })
 
 server.listen(4000)
