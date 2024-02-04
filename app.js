3accbc873fc7548cfcbcd38a35ba2cb301a4a263
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-04 21:01:09 +0530

    routing

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const http = require("http")
 
-const server = http.createServer((req,res)=>{
-    console.log("Sumit Singh Tomar")
-    // res.write("hello sumit singh tomar")
-    process.exit()
+const server = http.createServer((req, res) => {
+    if (req.url === '/node') {
+        res.setHeader("Content-Type", "text/html")
+        res.write("<html>")
+        res.write("<head><title>request and response page</title></head>")
+        res.write("<body><h1>Welcome to my node Js project</h1></body>")
+        res.write("</html>")
+        return res.end()
+    }
+    else if(req.url === '/home'){
+        res.setHeader("Content-Type", "text/html")
+        res.write("<html>")
+        res.write("<head><title>request and response page</title></head>")
+        res.write("<body><h1>Welcome Home</h1></body>")
+        res.write("</html>")
+        return res.end()
+    }
+    else if(req.url === '/about'){
+        res.setHeader("Content-Type", "text/html")
+        res.write("<html>")
+        res.write("<head><title>request and response page</title></head>")
+        res.write("<body><h1>Welcome to About Us Page</h1></body>")
+        res.write("</html>")
+        return res.end()
+    }
+    else{
+        res.setHeader("Content-Type", "text/html")
+        res.write("<html>")
+        res.write("<head><title>request and response page</title></head>")
+        res.write("<body>Go to Node Page <form action='/node' method='POST'><button type='submit'>click</button></form></body><br>")
+        res.write("<body>Go to home Page <form action='/home' method='POST'><button type='submit'>click</button></form></body><br>")
+        res.write("<body>Go to about Page <form action='/about' method='POST'><button type='submit'>click</button></form></body><br>")
+        res.write("</html>")
+        return res.end()   
+    }
 })
 
 server.listen(4000)
