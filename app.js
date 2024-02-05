f1d8c29d67adbd9729e2f4f1708bbd5fc3dabc77
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-05 22:35:51 +0530

    add routes file for clean up code

--- app.js
+++ app.js
@@ -1,66 +1,7 @@
 const http = require("http")
-const fs = require("fs")
+const routes = require("./routes")
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        var fileData = fs.readFileSync("hello.txt")
-        res.setHeader("Content-Type", "text/html")
-        res.write("<html>")
-        res.write("<head><title>create File</title></head>")
-        res.write(`${fileData}`)
-        res.write("<body><form action='/message' method='POST'><input type='text' name='message' /> <button type='submit'>Click</button> </form></body>")
-        res.write("</html>")
-        return res.end()
-    }
-    else if (req.url === '/message' && req.method === 'POST') {
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedbody = Buffer.concat(body).toString().split("=")[1]
-            console.log(parsedbody);
-            res.statusCode = 302;
-            res.setHeader("Location", "/")
-            fs.writeFileSync("hello.txt", parsedbody)
-            return res.end()
-        })
-    }
-    else if (req.url === '/node') {
-        res.setHeader("Content-Type", "text/html")
-        res.write("<html>")
-        res.write("<head><title>request and response page</title></head>")
-        res.write("<body><h1>Welcome to my node Js project</h1></body>")
-        res.write("</html>")
-        return res.end()
-    }
-    else if (req.url === '/home') {
-        res.setHeader("Content-Type", "text/html")
-        res.write("<html>")
-        res.write("<head><title>request and response page</title></head>")
-        res.write("<body><h1>Welcome Home</h1></body>")
-        res.write("</html>")
-        return res.end()
-    }
-    else if (req.url === '/about') {
-        res.setHeader("Content-Type", "text/html")
-        res.write("<html>")
-        res.write("<head><title>request and response page</title></head>")
-        res.write("<body><h1>Welcome to About Us Page</h1></body>")
-        res.write("</html>")
-        return res.end()
-    }
-    else {
-        res.setHeader("Content-Type", "text/html")
-        res.write("<html>")
-        res.write("<head><title>request and response page</title></head>")
-        res.write("<body>Go to Node Page <form action='/node' method='POST'><button type='submit'>click</button></form></body><br>")
-        res.write("<body>Go to home Page <form action='/home' method='POST'><button type='submit'>click</button></form></body><br>")
-        res.write("<body>Go to about Page <form action='/about' method='POST'><button type='submit'>click</button></form></body><br>")
-        res.write("</html>")
-        return res.end()
-    }
-})
+const server = http.createServer(routes)
 
 server.listen(4000)
 
