5975f477072721049db3f9db5b46940791c259be
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-06 15:46:34 +0530

    add express with middleware

--- app.js
+++ app.js
@@ -1,44 +1,24 @@
 const http = require("http")
-const routes = require("./routes")
+const express = require("express")
+const app = express()
 
-const server = http.createServer(routes)
+// adding middlware
+
+app.use((req, res, next) => {
+    console.log('middleware-1')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('middleware-2')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('middleware-3')
+    res.send("hello world form middleware three")
+})
+
+const server = http.createServer(app)
 
 server.listen(4000)
-
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/node') {
-//         res.setHeader("Content-Type", "text/html")
-//         res.write("<html>")
-//         res.write("<head><title>request and response page</title></head>")
-//         res.write("<body><h1>Welcome to my node Js project</h1></body>")
-//         res.write("</html>")
-//         return res.end()
-//     }
-//     else if(req.url === '/home'){
-//         res.setHeader("Content-Type", "text/html")
-//         res.write("<html>")
-//         res.write("<head><title>request and response page</title></head>")
-//         res.write("<body><h1>Welcome Home</h1></body>")
-//         res.write("</html>")
-//         return res.end()
-//     }
-//     else if(req.url === '/about'){
-//         res.setHeader("Content-Type", "text/html")
-//         res.write("<html>")
-//         res.write("<head><title>request and response page</title></head>")
-//         res.write("<body><h1>Welcome to About Us Page</h1></body>")
-//         res.write("</html>")
-//         return res.end()
-//     }
-//     else{
-//         res.setHeader("Content-Type", "text/html")
-//         res.write("<html>")
-//         res.write("<head><title>request and response page</title></head>")
-//         res.write("<body>Go to Node Page <form action='/node' method='POST'><button type='submit'>click</button></form></body><br>")
-//         res.write("<body>Go to home Page <form action='/home' method='POST'><button type='submit'>click</button></form></body><br>")
-//         res.write("<body>Go to about Page <form action='/about' method='POST'><button type='submit'>click</button></form></body><br>")
-//         res.write("</html>")
-//         return res.end()
-//     }
-// })
