75dd77c60e99e8b3587915588bd335bdbea37ced
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-06 17:06:46 +0530

    add body parser in  middleware

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/add-product', (req, res, next) => {
     console.log('middleware-2')
-    res.send("<form action='/user' method='POST'><input type='text' name='title' /> <button type='submit'>Send</button> </form>")
+    res.send("<form action='/user' method='POST'><input type='text' name='title' /> <input type='text' name='size' /> <button type='submit'>Send</button> </form>")
 })
 
 app.use('/user', (req, res, next) => {
     console.log(req.body)
-    res.redirect("/")
+    res.redirect("/add-product")
 })
 
 app.use('/', (req, res, next) => {
