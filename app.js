419cc440d25f549acd88cc6048dcba7a049f42ad
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-06 16:55:53 +0530

    add body parser middleware

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const http = require("http")
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
 
 // adding middlware
 
-app.use((req, res, next) => {
-    console.log('middleware-1')
-    next()
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('middleware-2')
-    next()
+    res.send("<form action='/user' method='POST'><input type='text' name='title' /> <button type='submit'>Send</button> </form>")
 })
 
-app.use((req, res, next) => {
-    console.log('middleware-3')
-    res.send("hello world form middleware three")
+app.use('/user', (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
 })
 
-const server = http.createServer(app)
+app.use('/', (req, res, next) => {
+    console.log('middleware-1')
+    res.send("<h1>Hello from middleware-1</h1>")
+})
 
-server.listen(4000)
+
+// app.use((req, res, next) => {
+//     console.log('middleware-3')
+//     res.send({key1:'value1'})
+// })
+
+// const server = http.createServer(app)
+
+app.listen(4000)
